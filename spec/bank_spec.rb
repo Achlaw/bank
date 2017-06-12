a35af4da519bb0c4074e387c152bb11959bb6866
Alex Law <agent_law@hotmail.com>
2017-06-12 11:07:30 +0100

    Add test stores money in bank balance

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -7,5 +7,12 @@ describe Bank do
     expect(bank.balance).to eq(0)
   end
 
+  describe '#deposit' do
+    it 'stores money in bank balance' do
+      bank = Bank.new
+      bank. deposit(100)
+      expect(bank.balance).to eq(100)
+    end
+  end
 
 end
