b2934e9100b8a003f5da122641192f638cc81d6b
Alex Law <achlawuk@gmail.com>
2017-06-13 00:50:51 +0100

    Require transaction to bank.rb

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,13 +1,17 @@
+require_relative 'transaction'
+
 class Bank
 
-  attr_reader :balance
+  attr_reader :balance, :transaction
 
-  def initialize
+  def initialize(balance =0, transaction = Transaction.new)
     @balance = 0
+    @transaction = transaction
   end
 
   def deposit(amount)
     @balance += amount
+    @transaction.add_deposit_history(amount, @balance)
   end
 
   def withdraw(amount)
