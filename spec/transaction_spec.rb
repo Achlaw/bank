3649bb878cb72a1a19f5de0bfa216cbddf4af18c
Alex Law <agent_law@hotmail.com>
2017-06-14 16:54:50 +0100

    Fixed broken tests due to date format

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,17 +1,17 @@
 require 'transaction'
 
 describe Transaction do
-  
+
   it 'adds date to deposit history' do
     transaction = Transaction.new
     transaction.add_deposit_history(100, 100)
-    expect(transaction.history).to eq([{Date: '13/06/2017', Credit: 100, Balance: 100}])
+    expect(transaction.history).to eq([{Date: "#{Time.new.strftime("%d/%m/%Y")}", Credit: 100, Balance: 100}])
   end
 
   it 'adds date to withdraw history' do
     transaction = Transaction.new
     transaction.add_withdraw_history(100, 0)
-    expect(transaction.history).to eq([{Date: '13/06/2017', Debit: 100, Balance: 0}])
+    expect(transaction.history).to eq([{Date: "#{Time.new.strftime("%d/%m/%Y")}", Debit: 100, Balance: 0}])
   end
 
 end
